f84682b2ddd90c7cdea1a1f0019fa05f49f8be4f
jakariya0dev <jhalhadi@gmail.com>
2025-06-13 20:50:59 +0600

    ft: search by price range added

--- src/pages/AllRooms.jsx
+++ src/pages/AllRooms.jsx
@@ -1,58 +1,122 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router";
 import RoomItemCard from "../components/common/RoomItemCard";
 
 export default function AllRooms() {
-
   const allRooms = useLoaderData();
+  const [sortType, setSortType] = useState("default");
   const [sortedRooms, setSortedRooms] = useState(allRooms);
+  const [minPrice, setMinPrice] = useState(10);
+  const [maxPrice, setMaxPrice] = useState(1000);
 
-  function handleSortChange(event) {
-    const sortValue = event.target.value;
-    let sorted = [...allRooms];
+  useEffect(() => {
+    axios
+      .get(
+        `${
+          import.meta.env.VITE_BASE_URL
+        }/rooms/price-range?minPrice=${minPrice}&maxPrice=${maxPrice}`
+      )
+      .then((res) => {
+        let data = res.data.rooms;
+        console.log(data);
 
-    if (sortValue === "default") {
-      setSortedRooms(allRooms);
-    } else if (sortValue === "price-asc") {
-      sorted.sort((a, b) => a.price - b.price);
-      setSortedRooms(sorted);
-    } else if (sortValue === "price-desc") {
-      sorted.sort((a, b) => b.price - a.price);
-      setSortedRooms(sorted);
-    } else if (sortValue === "rating-asc") {
-      sorted.sort((a, b) => b.rating - a.rating);
-      setSortedRooms(sorted);
-    } else if (sortValue === "rating-desc") {
-      sorted.sort((a, b) => a.rating - b.rating);
-      setSortedRooms(sorted);
-    }
-  }
+        if (sortType === "price-asc") {
+          data.sort((a, b) => a.price - b.price);
+        } else if (sortType === "price-desc") {
+          data.sort((a, b) => b.price - a.price);
+        } else if (sortType === "rating-asc") {
+          data.sort((a, b) => b.rating - a.rating);
+        } else if (sortType === "rating-desc") {
+          data.sort((a, b) => a.rating - b.rating);
+        }
+
+        setSortedRooms(data);
+      })
+      .catch((error) =>
+        console.error("Error fetching rooms by price range:", error)
+      );
+  }, [minPrice, maxPrice, sortType]);
+
+  const handleMinChange = (e) => {
+    const value = parseInt(e.target.value);
+    if (value < maxPrice) setMinPrice(value);
+  };
+
+  const handleMaxChange = (e) => {
+    const value = parseInt(e.target.value);
+    if (value > minPrice) setMaxPrice(value);
+  };
 
   return (
     <section className="py-16 px-4 bg-white">
       <div className="max-w-7xl mx-auto">
-        <div>
-          <label htmlFor="rooms">Sort By:</label>
-          <select
-            className="ml-2 p-1 border rounded"
-            id="rooms"
-            onChange={handleSortChange}
-          >
-            <option value="default">Default</option>
-            <option value="price-asc">Price Low to High</option>
-            <option value="price-desc">Price High to Low</option>
-            <option value="rating-asc">Rating High to Low</option>
-            <option value="rating-desc">Rating Low to High</option>
-          </select>
+        <div className="flex justify-between items-center mb-8">
+          {/* Sorting Options */}
+          <div>
+            <label htmlFor="rooms">Sort By:</label>
+            <select
+              className="ml-2 p-1 border rounded"
+              id="rooms"
+              onChange={(e) => setSortType(e.target.value)}
+            >
+              <option value="default">Default</option>
+              <option value="price-asc">Price Low to High</option>
+              <option value="price-desc">Price High to Low</option>
+              <option value="rating-asc">Rating High to Low</option>
+              <option value="rating-desc">Rating Low to High</option>
+            </select>
+          </div>
+
+          <div className="w-full max-w-sm bg-white p-4 rounded-lg shadow-md">
+            <label className="block text-lg font-semibold text-gray-800 mb-2">
+              Filter by Price Range
+            </label>
+
+            <div className="flex justify-between text-sm text-gray-600 mb-1"></div>
+
+            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+              <div className="w-full">
+                <label className="text-sm font-medium text-gray-700">
+                  Min Price: <span>${minPrice}</span>
+                </label>
+                <input
+                  type="range"
+                  min="10"
+                  max="1000"
+                  step="10"
+                  value={minPrice}
+                  onChange={handleMinChange}
+                  className="w-full accent-green-600"
+                />
+                <label className="text-sm font-medium text-gray-700">
+                  Max Price: <span>${maxPrice}</span>
+                </label>
+                <input
+                  type="range"
+                  min="10"
+                  max="1000"
+                  step="10"
+                  value={maxPrice}
+                  onChange={handleMaxChange}
+                  className="w-full accent-blue-600"
+                />
+              </div>
+            </div>
+          </div>
         </div>
         <h2 className="text-3xl font-bold text-center mb-10 text-gray-800">
           Rooms in Our Hotel
         </h2>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {sortedRooms.map((room) => (
-            <RoomItemCard key={room._id} room={room} />
-          ))}
+          {sortedRooms.length > 0 ? (
+            sortedRooms.map((room) => (
+              <RoomItemCard key={room._id} room={room} />
+            ))
+          ) : (
+            <div> No rooms found </div>
+          )}
         </div>
       </div>
     </section>
